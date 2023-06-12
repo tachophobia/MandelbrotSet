3d2504b421f8c45c70892a75245a437a2acbeb21
Nikita Lugovoy <niklugovoy@gmail.com>
2023-06-12 15:59:37 -0400

    finishing touches :)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import numpy as np
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageGrab
+from datetime import datetime
+from tkinter import messagebox
 
 try:
     import cv2
@@ -11,11 +13,17 @@ except ImportError as e:
 
 continue_running = True
 
+# hyperparameters
+dbail = 1e6
+iterations = 10000
+r, g, b = 1, 1, 1
+
+
 def mandelbrot(Z, max_iter):
+    global dbail
     z = np.zeros_like(Z)
     iters = np.zeros_like(Z, dtype=int)
     dz = np.ones_like(Z)
-    dbail = 1e6
 
     for _ in range(max_iter):
         mask = np.abs(dz) < dbail
@@ -39,12 +47,15 @@ def zoomOut(event):
 
 
 def reset(_):
-    global xmax, xmin, ymax, ymin, zoom, anchor, box
+    global xmax, xmin, ymax, ymin, zoom, anchor, box, r, g, b, dbail, iterations
     zoom = 1
     anchor = WIDTH//2, HEIGHT//2
     xmin, xmax, ymin, ymax = -2, 1, -1.3, 1.3
     last_state['index'] = 0
     box = False
+    r, g, b = 1, 1, 1
+    dbail = 1e6
+    iterations = 10000
     render_graphics()
 
 
@@ -106,7 +117,8 @@ def seizure_colors(iterations):
     return colors
 
 
-def aesthetic_colors(r, g, b):
+def aesthetic_colors():
+    global r, g, b
     iterations = 256
     colors = np.zeros((iterations, 3), dtype=int)
     for i in range(iterations):
@@ -118,18 +130,20 @@ def aesthetic_colors(r, g, b):
 
 
 def render_graphics():
-    global last_state, zoom, iterations, select_rect
+    global last_state, zoom, iterations, select_rect, display_stats
 
     gamestates = update_state(mandelbrot, iterations)
-    color_map_arr = aesthetic_colors(1, 1, 1)   # 1, 2, 3 for default
+    color_map_arr = aesthetic_colors()   # 1, 2, 3 for default
 
     for i, gamestate in enumerate(gamestates):
         if not continue_running:
             return
-        if (i + 1) % 2 == 0:
-            gamestate = gamestate.astype(np.float64)
-            gamestate *= 255./gamestate.max()
-            gamestate = gamestate.astype(np.uint8)
+
+        gamestate = gamestate.astype(np.float64)
+        gamestate *= 255./gamestate.max()
+        gamestate = gamestate.astype(np.uint8)
+
+        if not np.allclose(gamestate, np.ones_like(gamestate) * 255):
             gamestate_colored = color_map_arr[gamestate % iterations]
             gamestate_colored = gamestate_colored.astype(np.uint8)
 
@@ -141,8 +155,13 @@ def render_graphics():
                 last_state['index'] = i
 
             canvas.create_image(0, 0, image=last_state['state'], anchor=tk.NW)
+            if display_stats:
+                info = f"Zoom factor: {(2.6 / abs(ymax-ymin)):.0f}\nCoordinates: {round((xmax+xmin)/2, 3)} + {round((ymax+ymin)/2, 3)}i"
+                canvas.create_text(WIDTH // 1.3, int(HEIGHT * .05), text=info, anchor=tk.NW,
+                                   fill='red', font=("Arial", 20))
             canvas.update()
             root.update()
+
         if zoom != 1 and installed:
             last_state['index'] = 20
 
@@ -160,10 +179,11 @@ def render_graphics():
             root.update()
 
             break
+
         if last_state['reset']:
             last_state['reset'] = False
             break
-    
+
     root.after(16, render_graphics)
 
 
@@ -211,10 +231,87 @@ def end_selection(_):
     render_graphics()
 
 
+def set_display(_):
+    global display_stats
+    display_stats = not display_stats
+
+
+def capture_screenshot(_):
+    screenshot = ImageGrab.grab(bbox=(root.winfo_x(), root.winfo_y(
+    ), root.winfo_x() + WIDTH, root.winfo_y() + HEIGHT))
+    name = f"fractal-{datetime.now()}.png"
+    screenshot.save(name)
+    print(f"Screenshot captured and saved as '{name}'")
+
+
 def exit_program():
     global continue_running
     continue_running = False
     root.destroy()
+
+
+def set_hyperparameters():
+    global dbail, iterations, r, g, b
+
+    hyperparameters_window = tk.Toplevel(root)
+    hyperparameters_window.title("Set Hyperparameters")
+    hyperparameters_window.geometry(f"250x150+{WIDTH}+{HEIGHT}")
+
+    dbail_label = tk.Label(hyperparameters_window, text="dbail:")
+    dbail_label.grid(row=0, column=0)
+    dbail_entry = tk.Entry(hyperparameters_window)
+    dbail_entry.insert(0, str(dbail))
+    dbail_entry.grid(row=0, column=1)
+
+    iterations_label = tk.Label(hyperparameters_window, text="Iterations:")
+    iterations_label.grid(row=1, column=0)
+    iterations_entry = tk.Entry(hyperparameters_window)
+    iterations_entry.insert(0, str(iterations))
+    iterations_entry.grid(row=1, column=1)
+
+    r_label = tk.Label(hyperparameters_window, text="R:")
+    r_label.grid(row=2, column=0)
+    r_entry = tk.Entry(hyperparameters_window)
+    r_entry.insert(0, str(r))
+    r_entry.grid(row=2, column=1)
+
+    g_label = tk.Label(hyperparameters_window, text="G:")
+    g_label.grid(row=3, column=0)
+    g_entry = tk.Entry(hyperparameters_window)
+    g_entry.insert(0, str(g))
+    g_entry.grid(row=3, column=1)
+
+    b_label = tk.Label(hyperparameters_window, text="B:")
+    b_label.grid(row=4, column=0)
+    b_entry = tk.Entry(hyperparameters_window)
+    b_entry.insert(0, str(b))
+    b_entry.grid(row=4, column=1)
+
+    def update_hyperparameters():
+        global dbail, iterations, r, g, b
+
+        try:
+            dbail = float(eval(dbail_entry.get()))
+            iterations = int(eval(iterations_entry.get()))
+            r = float(r_entry.get())
+            g = float(g_entry.get())
+            b = float(b_entry.get())
+            hyperparameters_window.destroy()
+        except:
+            messagebox.showerror(
+                "Invalid Input", "Please enter valid numeric values for the hyperparameters.")
+        regen(None)
+
+    update_button = tk.Button(hyperparameters_window,
+                              text="Update", command=update_hyperparameters)
+    update_button.grid(row=5, columnspan=2)
+
+
+def show_menu(event):
+    menu = tk.Menu(root, tearoff=0)
+    menu.add_command(label="set hyperparameters", command=set_hyperparameters)
+    menu.post(event.x_root, event.y_root)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -229,15 +326,19 @@ if __name__ == "__main__":
     zoom = 1
     anchor = WIDTH//2, HEIGHT//2
     select_rect = None
+    display_stats = False
 
     root.protocol("WM_DELETE_WINDOW", exit_program)
     root.bind("<Escape>", exit_program)
+    root.bind("<Button-3>", show_menu)
     root.bind("<Button-4>", zoomIn)
     root.bind("<Key-z>", zoomIn)
     root.bind("<Button-5>", zoomOut)
     root.bind("<Key-x>", zoomOut)
     root.bind("<Key-r>", reset)
     root.bind("<Key-a>", regen)
+    root.bind("<KeyRelease-c>", set_display)
+    root.bind("<Key-s>", capture_screenshot)
     canvas.bind("<ButtonPress-1>", start_selection)
     canvas.bind("<B1-Motion>", update_selection)
     canvas.bind("<ButtonRelease-1>", end_selection)
@@ -246,7 +347,6 @@ if __name__ == "__main__":
                   'index': 0, 'anchor': anchor, 'reset': False}
     xmin, xmax, ymin, ymax = -2, 1, -1.3, 1.3
     threshold = False
-    iterations = 10000
     box = False
     render_graphics()
 
